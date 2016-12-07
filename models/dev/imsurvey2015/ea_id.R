09c7bf169d08a7f7c3a834fc95bff8b7c87658a4
peterhurford <peter@peterhurford.com>
2016-12-07 17:12:31 -0600

    Nothing to see here

--- models/dev/imsurvey2015/ea_id.R
+++ models/dev/imsurvey2015/ea_id.R
@@ -12,6 +12,8 @@ data2014[["Your email address"]] <- NULL
 data2014[["Giving your email address would let you revoke permissions you've given to make things public and edit your answers. It would also be much appreciated."]] <- NULL
 data2015[["Your name"]] <- NULL
 data2015[["Your email address"]] <- NULL
+data2015[["What is the web address of your EA Profile?"]] <- NULL
+data2015[["Are you sure you don't want to give your e-mail address? Please enter it here if you'd like."]] <- NULL
 
 readr::write_csv(data2014, "data/imsurvey2014-anonymized.csv")
 readr::write_csv(data2015, "data/imsurvey2015-anonymized.csv")

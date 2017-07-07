d256d2ac5922b534c5e2e1a53b5933ef55542652
peterhurford <peter@peterhurford.com>
2017-07-07 14:25:54 -0500

    Add error-catching to variable names; add city/country; Comment out some problems in 2015 survey

--- models/imsurvey2017/ea_id.R
+++ models/imsurvey2017/ea_id.R
@@ -2,10 +2,14 @@ Ramd::define("variable_names", function(variable_names) {
   data2017_ <- c("data/ea-survey-2017-confidential-no-anon.csv",
                  "data/ea-survey-2017-donations-only-confidential-no-anon.csv") %/>%
                  readr::read_csv
-  browser()
   data2017 <- data2017_ %/>% function(df) { plyr::rename(df, variable_names) }
   data2017 <- data2017 %/>% function(df) { df[, intersect(names(df), unlist(variable_names))] }
   data2017 <- data2017 %_>% plyr::rbind.fill
+  names_that_did_not_work <- setdiff(unlist(unname(variable_names)), names(data2017))
+  if (length(names_that_did_not_work) > 0) {
+    stop("Error: some variables did not import -- ",
+      paste0(names_that_did_not_work, collapse = ", "))
+  }
   data2017$ea_id <- data2017$email_address %/>% digest::digest %>% unlist
   data2017$email_address <- NULL
   readr::write_csv(data2017, "data/imsurvey2017-anonymized.csv")

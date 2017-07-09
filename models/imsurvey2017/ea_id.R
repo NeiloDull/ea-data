802d5dc8710135cac168c136cf87671220e08409
peterhurford <peter@peterhurford.com>
2017-07-09 14:04:03 -0500

    Fix variable import

--- models/imsurvey2017/ea_id.R
+++ models/imsurvey2017/ea_id.R
@@ -1,8 +1,9 @@
 Ramd::define("variable_names", function(variable_names) {
   data2017_ <- c("data/ea-survey-2017-confidential-no-anon.csv",
                  "data/ea-survey-2017-donations-only-confidential-no-anon.csv") %/>%
-                 readr::read_csv
-  data2017 <- data2017_ %/>% function(df) { plyr::rename(df, variable_names) }
+                 function(x) { suppressWarnings(readr::read_csv(x)) }
+  data2017 <- data2017_ %/>% function(df) {
+     suppressWarnings(suppressMessages(plyr::rename(df, variable_names))) }
   data2017 <- data2017 %/>% function(df) { df[, intersect(names(df), unlist(variable_names))] }
   data2017 <- data2017 %_>% plyr::rbind.fill
   names_that_did_not_work <- setdiff(unlist(unname(variable_names)), names(data2017))
@@ -12,5 +13,7 @@ Ramd::define("variable_names", function(variable_names) {
   }
   data2017$ea_id <- data2017$email_address %/>% digest::digest %>% unlist
   data2017$email_address <- NULL
+  message("Writing out...")
   readr::write_csv(data2017, "data/imsurvey2017-anonymized.csv")
+  message("Written...")
 })

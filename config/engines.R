7a06faee7dd6c04e716d9e63debdd331fe8ceb17
peterhurford <peter@peterhurford.com>
2017-09-10 13:52:48 -0500

    go back to github engine

--- config/engines.R
+++ config/engines.R
@@ -1,3 +1,2 @@
-#engine("modeling", type = "github", repo = "syberia/modeling.sy", version = "mungebits3", mount = TRUE)
-engine("modeling", type = "local", path = "~/dev/modeling.sy", mount = TRUE)
+engine("modeling", type = "github", repo = "syberia/modeling.sy", version = "mungebits3", mount = TRUE)
 engine("survey", type = "github", repo = "peterhurford/survey.sy", mount = TRUE)
